8eba35e7a81db4f053ee863094c814d02cec631f
Tahsin Ahmad <tahsinahmad2004@gmail.com>
2025-05-12 16:17:14 +0600

    lec-1 update

--- Lec-1/hello.py
+++
@@ -1,5 +0,0 @@
-x = 10
-y = 10
-z = x + y
-print("Hello World")
-print("Sum of x and y is:", z)
---
+++ first_lec.py
@@ -0,0 +1,82 @@
+# This is a comment
+
+'''
+This is a multiline comment 
+'''
+
+"""
+This is also a multi line comment 
+This 2nd line of this multiline comment 
+"""
+
+# Variable:
+
+x = 7
+# print(x)
+# print(type(x))
+
+'''
+integer - int()
+floating number - float()
+string - str()
+'''
+
+# bag = int(10)
+# bag = str("Tahsin")
+
+# Case Sensitive
+y = 10
+Y = 14
+
+# print(y)
+
+# a , b , c = 12, 10, 20
+# print(a)
+# print(b)
+#
+# e = f = d = 100
+# print(d)
+# print(e)
+
+# input
+# name = int(input("Enter your age: ")) # string format
+# fl = float(input("Enter a floating number: "))
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your last name: ")
+#
+# full_name = first_name + last_name
+# print(full_name)
+
+# Math
+
+# num1 = 10
+# num2 = 20
+# print(num1+num2)
+# print(num1*num2)
+# print(num1-num2)
+
+# Add +
+# Sub -
+# Muli *
+# Divi /
+
+# Practice Problem-1:
+# n1 , n2 = 10,20
+# n2, n1 = 10, 20
+# print(n1)
+
+# Datatypes:
+txt = "Textbook"
+num = 10
+
+# Number
+
+# Int >> float
+num_1 = 12
+# print(type(num_1))
+
+num_1 = float(12)
+print(num_1)
+print(type(num_1))
+
