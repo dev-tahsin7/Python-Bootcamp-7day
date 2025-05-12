3efb3a0ab8f9fd0804a31135c6bb667f027925d0
Tahsin Ahmad <tahsinahmad2004@gmail.com>
2025-05-13 01:38:48 +0600

    lec 2

---
+++ lec_2.py
@@ -0,0 +1,102 @@
+# String
+# name = "Python 7 days Free bootcamp"
+# print(name)
+
+# String usecase:
+# print("Welcome to our bootcamp!")
+
+# String Index :
+x = "Python" # access the char "h"
+# print(x[3])
+
+# Multiline str
+three_str = '''This is a multiline string 
+python 7 days free bootcamp'''
+
+three_double = """ this is also a multiline string
+hello world
+"""
+
+# print(three_double)
+
+# for loop in str
+
+# for i in "Tahsin":
+#     print(i)
+
+# use of len()
+# nam = "Python"
+# print(len(nam))
+
+# use of 'in' keyword
+# full_name = "Tahsin Ahmad"
+# print("Ahmed" in full_name)
+
+# Slicing
+
+a = "Tahsin"
+# access only 'sin'
+
+# print(a[3:])
+
+# Use case of slicing
+email = "tahsin123@gmail.com"
+# only take the username
+username = email[:email.index("@")]
+# print(username)
+
+# String Methods
+
+# upper()
+ab = "proxima it"
+# print(ab.upper())
+
+# lower()
+bc = "PROXIMA IT"
+# print(bc.lower())
+
+# strip()
+# xx = "Hello World "
+# print(xx.strip())
+
+# replace()
+# n = "Tahsin Ahmed"
+# print(n)
+# print(n.replace("e", "a"))
+
+# split()
+ppp = "Hello, World" # >> convert into list and spet by (,)
+# print(ppp.split(","))
+
+# use of +
+# first_name = input("Enter your firstname: ")
+# last_name = input("Enter your lastname: ")
+# age = int(input("Enter your age: "))
+#
+# full_name = first_name + " " + last_name
+# # f-sting
+# full_namef = f"Your full name is {full_name}. Your age is {age}."
+# print(full_namef)
+
+# vari = """This is a multiline string. \nThis is a new line"""
+# print(vari)
+
+# Take a string input from the user and print the reversed version of it.
+
+# from_user = input("Write anything: ") # >> Python
+# print(from_user[::-1])
+
+# Booleans >> True / False
+
+# print(10>9)
+# print(20<10)
+# print(10==21)
+
+# a = 10
+# b = 20
+#
+# if a > b:
+#     print("True")
+# else:
+#     print("False")
+
