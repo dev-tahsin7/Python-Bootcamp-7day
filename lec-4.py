63ba1107c7dcd6ea0377d359c7a206a8f31cae27
Tahsin Ahmad <tahsinahmad2004@gmail.com>
2025-05-18 15:07:38 +0600

    day-4

---
+++ lec-4.py
@@ -0,0 +1,67 @@
+# Lec- 4 :
+
+# conditions
+
+a = 30
+b = 30
+
+# a > b : print - true
+# a < b : print - false
+
+# if a > b or a == b:
+#     print("True")
+# elif a != b:
+#     print("True")
+# else:
+#     print("False")
+
+# x = 10
+# y = 20
+# z = 21
+#
+# if x == 10 :
+#     if y < z :
+#         print("True")
+#     else:
+#         print("False")
+# else:
+#     # print("Condition not match")
+#     pass
+
+# even / odd
+# user_input = int(input("Enter a number: "))
+#
+# if user_input % 2 == 0:
+#     print(f"{user_input} is an even number!")
+# else:
+#     print(f"{user_input} is an odd number")
+
+
+# ----------------------------------
+
+#Loop >> While Loop
+
+# i = 1
+# while i <= 100:
+#     print("I am Sorry Baby!!")
+#     if i == 25:
+#         break
+#     i += 1
+
+# Loop >> For Loop
+
+# this_list = ['Tahsin', 'Ahmad', 'Loop Labs']
+#
+# for i in this_list:
+#     print(i)
+#
+# txt = "LoopLabs"
+# for x in txt:
+#     print(x)
+
+# range()
+
+# for y in range(101):
+#     print(y)
+# else:
+#     print("Done!")
